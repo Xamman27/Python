86461ed137092081cc4270e2fc4ae7cdb7753e81
Xamman27 <124341155+Xamman27@users.noreply.github.com>
2023-02-22 21:47:01 +0600

    Add homework for module 4

---
+++ Module_4/4.2.py
@@ -0,0 +1,8 @@
+num_1 = int(input('Enter the first number: '))
+num_2 = int(input('Enter the second number: '))
+num_3 = int(input('Enter the third number: '))
+num_4 = int(input('Enter the fourth number: '))
+result_1 = num_1 + num_2
+result_2 = num_3 + num_4
+main_result = result_1 / result_2
+print('Calculation result', main_result)
---
+++ Module_4/4.6.py
@@ -0,0 +1,4 @@
+num_1 = int(input('Enter the first three-digit number: '))
+num_2 = int(input('Enter the second three-digit number: '))
+result = num_1 % 100 + num_2 % 100
+print('The result of the  Calculation is ', result)
